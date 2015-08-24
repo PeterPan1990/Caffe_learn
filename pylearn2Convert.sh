a28d78c9e250d759c983cc1eaca18c6da4942c69
Young <young.ouc@gmail.com>
2015-08-24 15:49:46 +0800

    create pylearn2Convert.sh

---
+++ pylearn2Convert.sh
@@ -0,0 +1,8 @@
+# use pylearn2 to proprocessing the dataset
+
+# 1.test the pylearn2 setup http://deeplearning.net/software/pylearn2/tutorial/index.html#tutorial
+
+# change to root
+su
+
+python make_dataset.p
